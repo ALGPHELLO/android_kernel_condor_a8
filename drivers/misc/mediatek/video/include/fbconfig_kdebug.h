a940eea2e069763a8dc3592c9f2faea8e58fe806
Khaled Necir <khaledannab@gmail.com>
2018-07-14 14:48:44 +0100

    Update fbconfig_kdebug.h
    add 
    #include "../mt6735/mt6753/ddp_ovl.h"

--- drivers/misc/mediatek/video/include/fbconfig_kdebug.h
+++ drivers/misc/mediatek/video/include/fbconfig_kdebug.h
@@ -9,7 +9,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- */
+ */ 
 
 #ifndef __FBCONFIG_KDEBUG_H
 #define __FBCONFIG_KDEBUG_H
