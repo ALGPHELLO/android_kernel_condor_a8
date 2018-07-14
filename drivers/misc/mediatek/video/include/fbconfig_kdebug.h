43f4ba8e16ec2c0368019bbd966981453ee533dc
Khaled Necir <khaledannab@gmail.com>
2018-07-14 14:47:38 +0100

    Update fbconfig_kdebug.h

--- drivers/misc/mediatek/video/include/fbconfig_kdebug.h
+++ drivers/misc/mediatek/video/include/fbconfig_kdebug.h
@@ -15,7 +15,7 @@
 #define __FBCONFIG_KDEBUG_H
 
 #include <linux/types.h>
-#include "ddp_ovl.h"
+#include "../mt6735/mt6753/ddp_ovl.h"
 
 void PanelMaster_Init(void);
 void PanelMaster_Deinit(void);
